d1cb62e193ea473b068c739b844ce6fa2524e13d
Milan Roberson <finagle29@gmail.com>
2020-08-24 17:22:02 -0700

    Move files into dbsp_drp directory to conform to Python packaging style
    Also adds setup.py and __init__.py to allow for installation.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="DBSP_DRP",
+    version="0.0.1dev",
+    description="Data Reduction Pipeline for Palomar's Double Spectrograph",
+    author="Milan S Roberson",
+    author_email="mroberso@caltech.edu",
+    license="GPL3",
+    packages=['dbsp_drp']
+)
