2d7dbcb602357280196bc9c6a7e5f0983f4d484c
Milan Roberson <finagle29@gmail.com>
2021-03-15 14:45:46 -0700

    mv test_p200_arm_redux.py test_splicing.py

--- dbsp_drp/tests/test_splicing.py
+++ dbsp_drp/tests/test_splicing.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from dbsp_drp import p200_arm_redux
+from dbsp_drp import splicing
 
 def interp_w_error_tester(xmin, xmax, xsize, xpmin, xpmax, xpsize):
     x = np.linspace(xmin, xmax, xsize)
@@ -8,7 +8,7 @@ def interp_w_error_tester(xmin, xmax, xsize, xpmin, xpmax, xpsize):
     yp = 1 - xp ** 2 + 1.1 * xp + np.sin(xp)
     err_yp = np.random.rand(xpsize)
 
-    y, yerr = p200_arm_redux.interp_w_error(x, xp, yp, err_yp)
+    y, yerr = splicing.interp_w_error(x, xp, yp, err_yp)
     y_numpy = np.interp(x, xp, yp)
     assert np.all(y == y_numpy)
 
