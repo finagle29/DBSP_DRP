bfeee1504655737fe97c6106ef06f0c013a44b44
Milan Roberson <finagle29@gmail.com>
2021-04-12 13:12:54 -0700

    first pass at splicing instrument factor

--- dbsp_drp/splicing.py
+++ dbsp_drp/splicing.py
@@ -3,7 +3,9 @@ Automated splicing for P200 DBSP.
 """
 
 import os
-from typing import Tuple, List
+from typing import Tuple, List, Optional
+import itertools
+import functools
 
 import numpy as np
 
@@ -16,6 +18,7 @@ import pypeit.pypeit
 from pypeit import msgs
 
 import dbsp_drp
+from dbsp_drp.instruments import Instrument
 
 def get_raw_header_from_coadd(coadd: str, root: str) -> str:
     if coadd is not None:
@@ -44,106 +47,114 @@ def get_raw_hdus_from_spec1d(spec1d_list: List[str], root: str, output_path: str
     return ret
 
 
-def splice(splicing_dict: dict, root: str, output_path: str) -> None:
+def splice(splicing_dict: dict, root: str, output_path: str, instrument: Instrument) -> None:
     """
-    Splices red and blue spectra together.
+    Splices spectra of all arms together.
     """
     for target, targets_dict in splicing_dict.items():
         label = 'a'
         for _, arm_dict in targets_dict.items():
-            ## TODO: refactor this to be instrument-agnostic
-            # splice probably needs instrument passed in?
-            red_dict = arm_dict.get('red', {})
-            blue_dict = arm_dict.get('blue', {})
+            arm_dicts = [arm_dict.get(arm, {}) for arm in instrument.arm_prefixes]
 
-            bluefile = blue_dict.get('coadd')
-            redfile = red_dict.get('coadd')
-            if bluefile is not None:
-                bluefile = os.path.join(output_path, 'Science', bluefile)
-            if redfile is not None:
-                redfile = os.path.join(output_path, 'Science', redfile)
-            if bluefile is None and redfile is None:
+            arm_files = [d.get('coadd') for d in arm_dicts]
+            arm_files = [
+                os.path.join(output_path, 'Science', arm_file) if arm_file is not None else None for arm_file in arm_files
+            ]
+
+            if not any(arm_files):
                 continue
 
             ((final_wvs, final_flam, final_flam_sig),
-                (red_wvs, red_flam, red_sig),
-                (blue_wvs, blue_flam, blue_sig)) = adjust_and_combine_overlap(bluefile, redfile, target)
+                (arm_wvs, arm_flams, arm_sigs)) = adjust_and_combine_overlap_all(arm_files, target)
 
             primary_header = fits.Header()
             primary_header['DBSP_DRP_V'] = dbsp_drp.__version__
             primary_header['PYPEIT_V'] = pypeit.__version__
             primary_header['NUMPY_V'] = np.__version__
             primary_header['ASTROPY_V'] = astropy.__version__
-            primary_header['B_COADD'] = bluefile
-            primary_header['R_COADD'] = redfile
+            for i, arm in instrument.arm_prefixes:
+                primary_header[f'{arm[0].upper()}_COADD'] = arm_files[i]
             primary_hdu = fits.PrimaryHDU(header=primary_header)
 
-            raw_red_hdus = get_raw_hdus_from_spec1d(red_dict.get('spec1ds', []), root, output_path)
-            raw_blue_hdus = get_raw_hdus_from_spec1d(blue_dict.get('spec1ds', []), root, output_path)
+            raw_arm_hdus = [get_raw_hdus_from_spec1d(d.get('spec1ds', []), root, output_path) for d in arm_dicts]
 
-
-            col_wvs = fits.Column(name='wave', array=red_wvs, unit='ANGSTROM', format='D')
-            col_flux = fits.Column(name='flux', array=red_flam, unit='E-17 ERG/S/CM^2/ANG', format='D')
-            col_error = fits.Column(name='sigma', array=red_sig, unit='E-17 ERG/S/CM^2/ANG', format='D')
-            red_hdu = fits.BinTableHDU.from_columns([col_wvs, col_flux, col_error], name="RED")
-
-            col_wvs = fits.Column(name='wave', array=blue_wvs, unit='ANGSTROM', format='D')
-            col_flux = fits.Column(name='flux', array=blue_flam, unit='E-17 ERG/S/CM^2/ANG', format='D')
-            col_error = fits.Column(name='sigma', array=blue_sig, unit='E-17 ERG/S/CM^2/ANG', format='D')
-            blue_hdu = fits.BinTableHDU.from_columns([col_wvs, col_flux, col_error], name="BLUE")
+            arm_hdus = [None] * instrument.arms
+            for i, arm in enumerate(instrument.arm_prefixes):
+                col_wvs = fits.Column(name='wave', array=arm_wvs[i], unit='ANGSTROM', format='D')
+                col_flux = fits.Column(name='flux', array=arm_flams[i], unit='E-17 ERG/S/CM^2/ANG', format='D')
+                col_error = fits.Column(name='sigma', array=arm_sigs[i], unit='E-17 ERG/S/CM^2/ANG', format='D')
+                arm_hdus[i] = fits.BinTableHDU.from_columns([col_wvs, col_flux, col_error], name=arm.upper())
 
             col_wvs = fits.Column(name='wave', array=final_wvs, unit='ANGSTROM', format='D')
             col_flux = fits.Column(name='flux', array=final_flam, unit='E-17 ERG/S/CM^2/ANG', format='D')
             col_error = fits.Column(name='sigma', array=final_flam_sig, unit='E-17 ERG/S/CM^2/ANG', format='D')
             table_hdu = fits.BinTableHDU.from_columns([col_wvs, col_flux, col_error], name="SPLICED")
 
-            hdul = fits.HDUList(hdus=[primary_hdu, *raw_red_hdus, *raw_blue_hdus, red_hdu, blue_hdu, table_hdu])
+            hdul = fits.HDUList(hdus=[primary_hdu, *itertools.chain(*raw_arm_hdus), *arm_hdus, table_hdu])
 
             log_msg = f"{target}_{label}.fits contains "
-            if redfile is None:
-                log_msg += f"{os.path.basename(bluefile)}"
-            elif bluefile is None:
-                log_msg += f"{os.path.basename(redfile)}"
-            else:
-                log_msg += f"{os.path.basename(redfile)} and {os.path.basename(bluefile)}"
+            log_msg += ' and '.join([os.path.basename(arm_file) for arm_file in arm_files if arm_file is not None])
             print(log_msg)
+
             hdul.writeto(os.path.join(output_path, "Science", f'{target}_{label}.fits'), overwrite=True)
             label = chr(ord(label) + 1)
 
-def adjust_and_combine_overlap(bluefile: str, redfile: str, target: str) -> Tuple[
+def adjust_and_combine_overlap_all(arm_files: List[str], target: str) -> Tuple[
             Tuple[np.ndarray, np.ndarray, np.ndarray],
-            Tuple[np.ndarray, np.ndarray, np.ndarray],
-            Tuple[np.ndarray, np.ndarray, np.ndarray]]:
-    ## TODO:
-    # refactor this to be instrument-agnostic:
-    # take in array of arms
+            Tuple[List[np.ndarray], List[np.ndarray], List[np.ndarray]]]:
     # TODO: propagate input masks
 
-    if bluefile is not None:
-        spec_b = fits.open(bluefile)[1].data
-        if redfile is None:
-            return ((spec_b['wave'], spec_b['flux'], spec_b['ivar'] ** -0.5),
-                (None, None, None),
-                (spec_b['wave'], spec_b['flux'], spec_b['ivar'] ** -0.5))
-    if redfile is not None:
-        spec_r = fits.open(redfile)[1].data
-        if bluefile is None:
-            return ((spec_r['wave'], spec_r['flux'], spec_r['ivar'] ** -0.5),
-                (spec_r['wave'], spec_r['flux'], spec_r['ivar'] ** -0.5),
-                (None, None, None))
+    specs = [None] * len(arm_files)
+    specs_present = []
+
+    # read in all data
+    for i, arm_file in enumerate(arm_files):
+        if arm_file is not None:
+            specs[i] = fits.open(arm_file)[1].data
+            specs_present.append(i)
+    # if only one of the specs is not None
+    if len(specs_present) == 1:
+        ix = specs_present[0]
+        return ((specs[ix]['wave'], specs[ix]['flux'], specs[ix]['ivar'] ** -0.5),
+            ([spec['wave'] if spec is not None else None for spec in specs],
+            [spec['flux'] if spec is not None else None for spec in specs],
+            [spec['ivar'] ** -0.5 if spec is not None else None for spec in specs]))
+
+    specs_for_reduction = [
+        (spec['wave'], spec['flux'], spec['ivar'] ** -0.5) if spec is not None else None for spec in specs
+    ]
+
+    adjust_and_combine_overlap_reduce = functools.partial(adjust_and_combine_overlap, target=target)
+    final_wvs, final_flam, final_flam_sig = functools.reduce(adjust_and_combine_overlap_reduce, specs_for_reduction)
+
+    return ((final_wvs, final_flam, final_flam_sig),
+            ([spec['wave'] if spec is not None else None for spec in specs],
+            [spec['flux'] if spec is not None else None for spec in specs],
+            [spec['ivar'] ** -0.5 if spec is not None else None for spec in specs]))
+
+def adjust_and_combine_overlap(bluer_spec: Optional[Tuple[np.ndarray, np.ndarray, np.ndarray]],
+        redder_spec: Optional[Tuple[np.ndarray, np.ndarray, np.ndarray]],
+        target: str = "") -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if bluer_spec is None:
+        return redder_spec
+    elif redder_spec is None:
+        return bluer_spec
+
+    spec_r_wave, spec_r_flux, spec_r_sig = redder_spec
+    spec_b_wave, spec_b_flux, spec_b_sig = bluer_spec
 
     # combination steps
-    overlap_lo = spec_r['wave'][0]
-    overlap_hi = spec_b['wave'][-1]
+    overlap_lo = spec_r_wave[0]
+    overlap_hi = spec_b_wave[-1]
     # maybe need to fix the overlaps?
     # need more finely spaced grid to be completely contained within coarser grid
 
-    olap_r = (spec_r['wave'] < overlap_hi)
-    olap_b = (spec_b['wave'] > overlap_lo)
+    olap_r = (spec_r_wave < overlap_hi)
+    olap_b = (spec_b_wave > overlap_lo)
 
     red_mult = 1
-    red_mult = (astropy.stats.sigma_clipped_stats(spec_b['flux'][olap_b])[1] /
-        astropy.stats.sigma_clipped_stats(spec_r['flux'][olap_r])[1])
+    red_mult = (astropy.stats.sigma_clipped_stats(spec_b_flux[olap_b])[1] /
+        astropy.stats.sigma_clipped_stats(spec_r_flux[olap_r])[1])
     #red_mult = np.average(spec_aag[1].data['OPT_FLAM'][olap_b], weights=spec_aag[1].data['OPT_FLAM_SIG'][olap_b] ** -2.0)\
     #   /np.average(spec_aag_red[1].data['OPT_FLAM'][olap_r], weights=spec_aag_red[1].data['OPT_FLAM_SIG'][olap_r] ** -2.0)
     if red_mult > 3 or 1/red_mult > 3:
@@ -152,20 +163,20 @@ def adjust_and_combine_overlap(bluefile: str, redfile: str, target: str) -> Tupl
 
 
     # different dispersion.
-    wvs_b = spec_b['wave'][~olap_b]
-    wvs_r = spec_r['wave'][~olap_r]
-    flam_b = spec_b['flux'][~olap_b]
-    flam_r = spec_r['flux'][~olap_r]
-    flam_sig_b = spec_b['ivar'][~olap_b] ** -0.5
-    flam_sig_r = spec_r['ivar'][~olap_r] ** -0.5
+    wvs_b = spec_b_wave[~olap_b]
+    wvs_r = spec_r_wave[~olap_r]
+    flam_b = spec_b_flux[~olap_b]
+    flam_r = spec_r_flux[~olap_r]
+    flam_sig_b = spec_b_sig[~olap_b]
+    flam_sig_r = spec_r_sig[~olap_r]
 
 
-    olap_wvs_r = spec_r['wave'][olap_r]
-    olap_flam_r = red_mult * spec_r['flux'][olap_r]
-    olap_flam_sig_r = red_mult * spec_r['ivar'][olap_r] ** -0.5
-    olap_wvs_b = spec_b['wave'][olap_b][:-1]
-    olap_flam_b = spec_b['flux'][olap_b][:-1]
-    olap_flam_sig_b = spec_b['ivar'][olap_b][:-1] ** -0.5
+    olap_wvs_r = spec_r_wave[olap_r]
+    olap_flam_r = red_mult * spec_r_flux[olap_r]
+    olap_flam_sig_r = red_mult * spec_r_sig[olap_r]
+    olap_wvs_b = spec_b_wave[olap_b][:-1]
+    olap_flam_b = spec_b_flux[olap_b][:-1]
+    olap_flam_sig_b = spec_b_sig[olap_b][:-1]
 
     olap_flam_r_interp = np.interp(olap_wvs_b, olap_wvs_r, olap_flam_r)
     olap_flam_r_interp, olap_flam_sig_r_interp = interp_w_error(olap_wvs_b, olap_wvs_r, olap_flam_r, olap_flam_sig_r)
@@ -181,9 +192,7 @@ def adjust_and_combine_overlap(bluefile: str, redfile: str, target: str) -> Tupl
     final_flam = np.concatenate((flam_b, olap_flam_avgd, red_mult * flam_r))
     final_flam_sig = np.concatenate((flam_sig_b, olap_flam_sig_avgd, red_mult * flam_sig_r))
 
-    return ((final_wvs, final_flam, final_flam_sig),
-        (spec_r['wave'], spec_r['flux'], spec_r['ivar'] ** -0.5),
-        (spec_b['wave'], spec_b['flux'], spec_b['ivar'] ** -0.5))
+    return (final_wvs, final_flam, final_flam_sig)
 
 def interp_w_error(x: np.ndarray, xp: np.ndarray, yp: np.ndarray, err_yp: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     if len(xp) == 1:
