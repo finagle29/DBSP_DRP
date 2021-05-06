072a609477685431ad8bb8f64adade157e0cd772
Milan Roberson <finagle29@gmail.com>
2021-05-06 15:56:09 -0700

    add --rm to docker_run

--- bin/run_docker.sh
+++ bin/run_docker.sh
@@ -21,11 +21,11 @@ fi
 if [[ $OSTYPE == "darwin"* ]]; then
     IP=`ifconfig en0 | grep 'inet ' | cut -d " " -f2`
     xhost +$IP
-    docker run -t -i --net=host -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix "$@"
+    docker run --rm -t -i --net=host -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix "$@"
 elif [[ $OSTYPE == "linux"* ]]; then
 # elif [[ `cat /etc/issue` == "Ubuntu*"]]; then
 # not sure if this /needs/ to be ubuntu
-    docker run -t -i --net=host -e DISPLAY --volume="$HOME/.Xauthority:/root/.Xauthority:rw" "$@"
+    docker run --rm -t -i --net=host -e DISPLAY --volume="$HOME/.Xauthority:/root/.Xauthority:rw" "$@"
 elif [[ $OSTYPE == "win"* ]]; then
     echo "You'll have to try to launch Docker on your own, sorry."
 fi
